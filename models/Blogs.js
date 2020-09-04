079542c051199199da9bd7d7c825a756916c0bc2
Gaurav Lohia <grvlohia6@gmail.com>
2020-09-04 12:52:52 +0530

    modified Blog model to include date of creation

--- models/Blogs.js
+++ models/Blogs.js
@@ -3,7 +3,8 @@ var mongoose = require('mongoose');
 var blogSchema = new mongoose.Schema({
     title: String,
     image: String,
-    content: String
+    content: String,
+    date: Date
 });
 
 module.exports = mongoose.model("Blog", blogSchema);
