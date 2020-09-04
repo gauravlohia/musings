bcd0c631987d681e7d77ef724dd00fbfd6f8a9ff
Gaurav Lohia <grvlohia6@gmail.com>
2020-09-04 12:51:31 +0530

    added stylesheet file for the app

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 5000;
 var mongoose = require('mongoose');
+var path = require('path');
 
 // REQUIRING ROUTES
 var blogRoutes = require("./routes/blogs");
@@ -13,11 +14,13 @@ mongoose.connect("mongodb://127.0.0.1:27017/musingsdb", {useNewUrlParser: true, 
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, "public")));
 
 // App Routes
 app.get("/", (req, res) => {
     res.render("index");
 });
+
 
 // USING IMPORTED ROUTES
 app.use(blogRoutes);
