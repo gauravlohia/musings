49ff8fc80daf2fffb6abdbcf2195f6ecd66ee20e
Gaurav Lohia <grvlohia6@gmail.com>
2020-09-04 12:55:24 +0530

    created index route for blogs and edited create route to include date of creation of the blog

--- routes/blogs.js
+++ routes/blogs.js
@@ -2,6 +2,19 @@ var express = require('express'),
     Blog = require('../models/Blogs'),
     router = express.Router();
 
+
+// INDEX ROUTE 
+router.get("/blogs", (req, res) => {
+    Blog.find({}, (err, foundBlogs) => {
+        if (err) {
+            console.log(err);
+        } else {
+            // console.log(foundBlogs);
+            res.render("blogs/index", {blogs: foundBlogs});
+        }
+    });
+});
+
 // NEW ROUTE - show the form to create a new blog
 router.get("/blogs/new", (req, res) => {
     res.render("blogs/new");
@@ -9,6 +22,8 @@ router.get("/blogs/new", (req, res) => {
 
 // CREATE route - create a new blog
 router.post("/blogs", (req, res) => {
+    var newBlog = req.body.blog;
+    newBlog.date = new Date();
     Blog.create(req.body.blog, (err, blog) => {
         if(err) {
             console.log(err);
